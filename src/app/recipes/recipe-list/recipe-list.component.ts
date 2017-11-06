42a08a675fcc029eb2ceed68d90d5c2b36f3c0e8
Midnighter <midnighter@posteo.net>
2017-11-06 01:17:25 +0100

    feat: add a recipe service prototype

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-
-import { Recipe } from '../recipe.model';
+import {Component, OnInit} from '@angular/core';
+import {RecipeService} from '../recipe.service';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,27 +8,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() detailFired = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      'Apple Cake',
-      'Make a delicious apple cake.',
-      'http://maxpixel.freegreatpicture.com/static/photo/1x/Food-Sweet-Baked-Italian-Apple-Cake-688119.jpg'
-    ),
-    new Recipe(
-      'Chocolate Cake',
-      'Make a delicious chocolate cake.',
-      'https://pixnio.com/free-images/food-and-drink/desserts-cakes/chocolate-dessert-cake-725x544.jpg'
-    )
-  ];
+  recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onDetail(item: Recipe) {
-    this.detailFired.emit(item);
+    this.recipes = this.recipeService.recipes;
   }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,28 @@
+// import { Injectable } from '@angular/core';
+import {Recipe} from './recipe.model';
+import {EventEmitter, Output} from '@angular/core';
+
+// @Injectable()
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Apple Cake',
+      'Make a delicious apple cake.',
+      'http://maxpixel.freegreatpicture.com/static/photo/1x/Food-Sweet-Baked-Italian-Apple-Cake-688119.jpg'
+    ),
+    new Recipe(
+      'Chocolate Cake',
+      'Make a delicious chocolate cake.',
+      'https://pixnio.com/free-images/food-and-drink/desserts-cakes/chocolate-dessert-cake-725x544.jpg'
+    )
+  ];
+  @Output() detailFired = new EventEmitter<Recipe>();
+
+  constructor() { }
+
+  select(idx: number) {
+    this.detailFired.emit(this.recipes[idx]);
+  }
+
+}
